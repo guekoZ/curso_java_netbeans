7b9938cfbdb1e3b0c34d93c3bb2cb6686881b877
gueko <gueko@gueko-pc>
2024-09-13 21:06:09 -0600

    segundo cambio en pc

--- src/main/java/com/mycompany/estudiojava/Estudiojava.java
+++ src/main/java/com/mycompany/estudiojava/Estudiojava.java
@@ -12,5 +12,7 @@ public class Estudiojava {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hola como estas");
+      
     }
 }
