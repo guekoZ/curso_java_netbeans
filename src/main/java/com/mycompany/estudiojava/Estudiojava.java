4b5ecae7dca2f0c0b12edc628c7c17833546daca
gueko1 <gueko1@gmail.com>
2024-10-26 02:43:58 -0600

    2do Cambio de Laptop 26/10/2024

--- src/main/java/com/mycompany/estudiojava/Estudiojava.java
+++ src/main/java/com/mycompany/estudiojava/Estudiojava.java
@@ -13,7 +13,7 @@ public class Estudiojava {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hola como estas");
-        System.out.println("com.mycompany.estudiojava.Estudiojava.main()");
+   
       
     }
 }
