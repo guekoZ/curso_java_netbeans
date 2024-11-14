7885a9967e791a3febafb2b195d076cd17c6babf
guekoZ <gueko1@gmail.com>
2024-11-13 21:00:51 -0600

    1er Cambio de PC 13/11/2024

---
+++ src/principal/Ejecuta_Modelo_Vista_Controlador.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package principal;
+
+import javax.swing.JFrame;
+import vista.Marco_Aplicacion2;
+
+/**
+ *
+ * @author gueko
+ */
+public class Ejecuta_Modelo_Vista_Controlador {
+    
+    public static void main(String[] args) {
+        
+        Marco_Aplicacion2 mimarco = new Marco_Aplicacion2();
+        mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mimarco.setVisible(true);
+        
+    }
+    
+}
