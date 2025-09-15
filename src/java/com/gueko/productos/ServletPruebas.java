063397e47ca3b8e276f6eb671daeccf3b0df4c06
gueko1 <gueko1@gmail.com>
2025-09-15 09:10:13 -0600

    1er cambio de laptop 06/09/25

---
+++ src/java/com/gueko/productos/ServletPruebas.java
@@ -0,0 +1,108 @@
+/*
+Este codigo se explica en los vides 250 y es sobre como realizar un CRUD
+ */
+package com.gueko.productos;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import java.sql.Connection;
+import java.sql.*;
+import java.sql.ResultSet;
+
+
+
+/**
+esta es la clase principal en donde se tinen los metodos doGet y doPost
+ */
+
+
+@WebServlet(name = "ServletPruebas", urlPatterns = {"/ServletPruebas"})
+public class ServletPruebas extends HttpServlet {
+    
+    private static final long serialVersionUID = 1L; 
+    
+    /**
+     Se define el pool de conexiones */
+    @Resource(name="jdbc/Productos")
+    
+    private DataSource miPool;
+    
+    
+    public ServletPruebas() {
+        super();
+        
+    }
+
+ 
+   
+
+    
+    /*
+    
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        //Crear el objeto printWriter
+        
+        PrintWriter salida=  response.getWriter();
+        
+        response.setContentType("text/plain");
+        
+        // Crear la conexion a la BBDD
+        
+        Connection miConexion = null;
+        Statement miStatement= null;
+        ResultSet miResultset = null;   
+              
+       try {
+       miConexion=miPool.getConnection();
+       String miSql= "SELECT * FROM productos";
+       
+       miStatement= miConexion.createStatement();
+       
+       miResultset=miStatement.executeQuery(miSql);
+       
+       while(miResultset.next()){
+           
+           String nombreArticulo= miResultset.getString(3);
+           System.out.println(nombreArticulo);
+       
+       }
+       
+       }catch(SQLException e){
+           System.out.println("Ocurrio un error");
+       }
+       }
+         
+                      
+    
+
+    /*     
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
